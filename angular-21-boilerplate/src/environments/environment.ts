ddf6a6308667513493f2814a4de7b7b9597a552e
Arvy Sabalande <155871828+arveyy@users.noreply.github.com>
2026-05-18 21:40:27 +0800

    updates environment

--- angular-21-boilerplate/src/environments/environment.ts
+++ angular-21-boilerplate/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:4000'
+  production: true,
+  apiUrl: 'https://ipt-2026-backend-p47t.onrender.com'
 };
 
 /*
